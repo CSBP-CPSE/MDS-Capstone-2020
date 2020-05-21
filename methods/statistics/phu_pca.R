ae3dafa73568612fa718703ac6cada172b13fa84
kt <kthobbs@mail.ubc.ca>
2020-05-21 14:41:26 -0700

    corrected merge of comorbidities and prox

--- methods/statistics/phu_pca.R
+++ methods/statistics/phu_pca.R
@@ -3,50 +3,50 @@
 #################################
 
 # read and clean data ----
-phu <- read.csv('../data/covid_comorbidities.csv')
-phu <- phu[,-1]
+phu <- read.csv('../data/PMD-en/PHU_FINAL.csv')
+phu <- phu[,-c(1,2)]
 phu[,c(3,6,9,10)] <- lapply(phu[,c(3,6,9,10)], function(x) as.numeric(gsub("[\\%,]", "", x)))
 phu[,c(4,5,7,8)] <- lapply(phu[,c(4,5,7,8)], function(x) as.numeric(gsub("[,]", "", x)))
 
+# PHU numerical for pca ---
+phu_num <- phu[,c(2,4,5,7,8,11:17, 40, 42, 44, 46, 48, 50, 52, 54, 56, 58, 60)]
+colnames(phu_num)
 
 # general analysis ----
-# n < p
-dim(phu) 
+# n > p
+dim(phu_num) 
 
-# missing values
-colSums(is.na(phu))
+# set missing values to 0
+colSums(is.na(phu_num))
+
+phu_num[is.na(phu_num)] <- 0
+
 
 # zero-variance columns 
-which(apply(phu, 2, var)==0)
+which(apply(phu_num, 2, var)==0)
       
 # PCA ----
 
 # remove zero-variance columns for scaling
-phu2 <- phu[ , which(apply(phu, 2, var) != 0)]
-phu_pca <- prcomp(phu2[,-c(1,2,16, 17, 18,19)], scale.=TRUE)
+phu_num2 <- phu_num2[, which(apply(phu, 2, var) != 0)]
+colnames(phu_num)
+phu_pca <- prcomp(phu_num2[,-1], scale.=TRUE)
 summary(phu_pca)
 
 # preserve 99% variation
-phu_pca$rotation[,1:5]
+phu_pca$rotation[,1:4]
 
 # plot
 plot(phu_pca, type="lines")
 
 biplot(phu_pca)
 
-############
-# mixPCA ----
-############
-
-library('PCAmixdata')
-data <- phu[, -c(1,19,20,21,22)]
-groups <- colnames(data)
-name.groups <- levels(phu$Reporting_PHU)
-
-MFAmix(data, groups, name.groups, ndim=5, rename.level=FALSE, graph = TRUE,
-       axes = c(1, 2))
-
 ################
 # Clustering ---
 ################
+
+
+###############
+# FA ----------
+###############
 

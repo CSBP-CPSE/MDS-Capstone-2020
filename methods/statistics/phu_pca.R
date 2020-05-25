18600f244f6d3fa432d9868658cbcac4d8a5b51f
kt <kthobbs@mail.ubc.ca>
2020-05-25 12:57:30 -0700

    with census calcs and cleaned

--- methods/statistics/phu_pca.R
+++ methods/statistics/phu_pca.R
@@ -1,6 +1,9 @@
 #################################
 ## COVID PHU-LEVEL PCA ----
 #################################
+# packages ----
+library('dplyr')
+library('tidyverse')
 
 # read and clean data ----
 phu <- read.csv('../data/PMD-en/PHU_FINAL.csv')
@@ -15,17 +18,18 @@ como_num <- c('copd', 'asthma', 'smokers', 'hbd')
 phu[como_num] <- lapply(phu[como_num], function(x) as.numeric(gsub("[,]", "", x)))
 
 # PHU numerical for pca ----
-keep <- c('Reporting_PHU', 'asthma', 'copd', 'hbd', 'smokers', 
+keep <- c('Reporting_PHU', '^asthma$', '^copd$', '^hbd$', '^smokers$', 
          'FEMALE', 'MALE', 'TRANSGENDER','OTHER',
          'CONTACT', 'NEITHER', 'TRAVEL.RELATED',
          'prox_idx_emp', 'prox_idx_pharma', 'prox_idx_childcare', 'prox_idx_health',
          'prox_idx_grocery', 'prox_idx_educpri', 'prox_idx_educsec', 'prox_idx_lib',
-         'prox_idxx_parks', 'prox_idx_transit', 'transit_na', 'suppreessed')
+         'prox_idxx_parks', 'prox_idx_transit', 'transit_na', 'suppressed')
 
-phu[colnames(phu) == keep]
-colnames(phu)
 
-phu_num <- phu[colnames(phu) == keep]
+phu_num <- phu %>% 
+              select(matches(keep))
+
+
 colnames(phu_num)
 
 # general analysis ----
@@ -35,16 +39,19 @@ dim(phu_num)
 # assess missing values
 colSums(is.na(phu_num))
 
-phu_num$prox_idx_parks[is.na(phu_num$prox_idx_parks)]
-phu_num$prox_idx_parks
-# zero-variance columns 
-which(apply(phu_num, 2, var)==0)
-      
+###########    
 # PCA ----
+###########
+# assess zero-variance columns 
+which(apply(phu_num, 2, var)==0)
 
-# remove zero-variance columns for scaling
-phu_num2 <- phu_num2[, which(apply(phu, 2, var) != 0)]
-colnames(phu_num2)
+lapply(phu_num[,3:length(phu_num)], function(x) var(x))
+
+# adjust scale of proximity measures or other columns
+
+
+#phu_num2 <- phu_num2[, which(apply(phu, 2, var) != 0)]
+#colnames(phu_num2)
 
 # pca - columns removed with little var ---
 phu_pca <- prcomp(phu_num2[,-1], scale.=TRUE)
@@ -59,16 +66,45 @@ plot(phu_pca, type="lines")
 biplot(phu_pca)
 
 
-###################
+## TO INVESTIGATE:
+# how to deal with variables that have significantly small variances,
+# especially relative to other variables
 
 
 
-################
-# Clustering ---
-################
+############################
+# Supervised Clustering ---
+############################
+clustering <- cbind(phu_num, phu$TOTAL)
+phu_clust <- hclust(dist(clustering$`phu$TOTAL`)) # clustering by TOTAL
+plot(phu_clust)
+
+# assuming 3 groups (although number 2 may be an outlier)
+cgroups <- cutree(phu_clust,3)
+phu[cgroups == 2, 1:2] # Toronto  # NOTE: only grab important cols from PCA
+
+phu[cgroups == 1, 1:2] # dense areas
+
+phu[cgroups == 3, 1:2] # everywhere else in ON
+
+# TO INVESTIGATE:
+# which col is reasonable to cluster by - maybe unsupervised clust is better 
+# which cols are important to observe (noted by PCA results)
+
+############################
+# Unsupervised Clustering ----
+# EM Model
+############################
+install.packages('EMCluster')
+library('EMCluster')
+
+emobj <- init.EM()
+emcluster(phu_num[, 3:length(phu_num)], emobj = NULL, pi = NULL, Mu = NULL, LTSigma = NULL,
+          lab = NULL, EMC = .EMC, assign.class = FALSE)
+
+##############################################
+# FA for connectedness (proximity data) ----
+##############################################
 
 
-###############
-# FA ----------
-###############
 

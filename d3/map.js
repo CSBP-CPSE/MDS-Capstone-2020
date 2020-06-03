b85db7df72c042dc9dce437ddfc940966983e7b8
kt <kthobbs@mail.ubc.ca>
2020-06-03 15:49:39 -0700

    metadata for map

--- d3/map.js
+++ d3/map.js
@@ -1,6 +1,3 @@
-// This is where the map will be coded.
-console.log("Ready to create the map!");
-
 // We specify the dimensions for the map container. We use the same
 // width and height as specified in the CSS above.
 var width = 900,
@@ -16,75 +13,32 @@ var svg = d3.select('#map').append('svg')
 //     https://github.com/mbostock/d3/wiki/Geo-Projections
 // and set the initial zoom to show the features.
 var projection = d3.geo.mercator()
-  // The approximate scale factor
-  .scale(1);
+  // The approximate scale factor was found through try and error
+  .scale(10000)
+  // The geographical center of Switzerland is around 46.8°, 8.2°
+  //     https://de.wikipedia.org/wiki/Älggi-Alp
+  .center([85.3232, 51.2538])
+  // Translate: Translate it to fit the container
+  .translate([width/2, height/2]);
 
 // We prepare a path object and apply the projection to it.
 var path = d3.geo.path()
   .projection(projection);
 
-
 // Load the features from the GeoJSON.
 d3.json('data/PHU.geojson', function(error, features) {
 
-  // Get the scale and center parameters from the features.
-  var scaleCenter = calculateScaleCenter(features);
-
-  // Apply scale, center and translate parameters.
-  projection.scale(scaleCenter.scale)
-    .center(scaleCenter.center)
-    .translate([width/2, height/2]);
-
-    // We add a <g> element to the SVG element and give it a class to
+  // We add a <g> element to the SVG element and give it a class to
   // style it later.
   svg.append('g')
       .attr('class', 'features')
-      // D3 wants us to select the (non-existing) path objects first ...
-      .selectAll('path')
+    // D3 wants us to select the (non-existing) path objects first ...
+    .selectAll('path')
       // ... and then enter the data. For each feature, a <path> element
       // is added.
       .data(features.features)
-      .enter().append('path')
+    .enter().append('path')
       // As "d" attribute, we set the path of the feature.
       .attr('d', path);
 
 });
-
-
-/**
- * Calculate the scale factor and the center coordinates of a GeoJSON
- * FeatureCollection. For the calculation, the height and width of the
- * map container is needed.
- *
- * Thanks to: http://stackoverflow.com/a/17067379/841644
- *
- * @param {object} features - A GeoJSON FeatureCollection object
- *   containing a list of features.
- *
- * @return {object} An object containing the following attributes:
- *   - scale: The calculated scale factor.
- *   - center: A list of two coordinates marking the center.
- */
-function calculateScaleCenter(features) {
-  // Get the bounding box of the paths (in pixels!) and calculate a
-  // scale factor based on the size of the bounding box and the map
-  // size.
-  var bbox_path = path.bounds(features),
-      scale = 0.95 / Math.max(
-        (bbox_path[1][0] - bbox_path[0][0]) / width,
-        (bbox_path[1][1] - bbox_path[0][1]) / height
-      );
-
-  // Get the bounding box of the features (in map units!) and use it
-  // to calculate the center of the features.
-  var bbox_feature = d3.geo.bounds(features),
-      center = [
-        (bbox_feature[1][0] + bbox_feature[0][0]) / 2,
-        (bbox_feature[1][1] + bbox_feature[0][1]) / 2];
-
-  return {
-    'scale': scale,
-    'center': center
-  };
-
-}

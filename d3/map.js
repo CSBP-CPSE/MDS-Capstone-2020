ec5613c14538cd05e942f6da02f0beb504f7791c
kt <kthobbs@mail.ubc.ca>
2020-06-03 20:49:00 -0700

    attempt at feedback

--- d3/map.js
+++ d3/map.js
@@ -12,21 +12,29 @@ var svg = d3.select('#map').append('svg')
 // We define a geographical projection
 //     https://github.com/mbostock/d3/wiki/Geo-Projections
 // and set the initial zoom to show the features.
-var projection = d3.geo.mercator()
-  // The approximate scale factor was found through try and error
-  .scale(10000)
-  // The geographical center of Switzerland is around 46.8°, 8.2°
-  //     https://de.wikipedia.org/wiki/Älggi-Alp
-  .center([85.3232, 51.2538])
-  // Translate: Translate it to fit the container
-  .translate([width/2, height/2]);
+var projection = d3.geoMercator();
 
 // We prepare a path object and apply the projection to it.
-var path = d3.geo.path()
+var path = d3.geoPath()
   .projection(projection);
 
+
+
 // Load the features from the GeoJSON.
-d3.json('data/PHU.geojson', function(error, features) {
+d3.json('data/PHU.json', function(error, features) {
+
+
+// rewinding the polygons in case they were counter-clockwise (to avoid the black image).
+  // var feat = features.features
+  //
+  // var fixed = feat.map(function(f) {
+  // 	return turf.rewind(f,{reverse:true});
+  // })
+
+  // console.log(fixed);
+
+
+  projection.fitSize([width,height], features)
 
   // We add a <g> element to the SVG element and give it a class to
   // style it later.
@@ -36,7 +44,7 @@ d3.json('data/PHU.geojson', function(error, features) {
     .selectAll('path')
       // ... and then enter the data. For each feature, a <path> element
       // is added.
-      .data(features.features)
+      .data(features.features) // was features.features
     .enter().append('path')
       // As "d" attribute, we set the path of the feature.
       .attr('d', path);

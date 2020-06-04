dddb899b476480742e94f1ff1a47893f430bc335
kt <kthobbs@mail.ubc.ca>
2020-06-04 12:01:39 -0700

    updated projections

--- d3/map.js
+++ d3/map.js
@@ -1,3 +1,4 @@
+console.log('Ready to make map!');
 // We specify the dimensions for the map container. We use the same
 // width and height as specified in the CSS above.
 var width = 900,
@@ -9,20 +10,25 @@ var svg = d3.select('#map').append('svg')
   .attr('width', width)
   .attr('height', height);
 
+console.log('Selecting map with d3.');
+
 // We define a geographical projection
 //     https://github.com/mbostock/d3/wiki/Geo-Projections
 // and set the initial zoom to show the features.
-var projection = d3.geoMercator();
-
-// We prepare a path object and apply the projection to it.
-var path = d3.geoPath()
-  .projection(projection);
+var projection = d3.geo.azimuthalEqualArea()
+    .rotate([100, -45])
+    .center([5, 20])
+    .scale(800)
+    .translate([width/2, height/2])
+    
+var path = d3.geo.path()
+.projection(projection);
 
 
 
 // Load the features from the GeoJSON.
-d3.json('data/PHU.json', function(error, features) {
-
+d3.json('data/PHU.json', function(ontario) {
+  console.log(ontario);
 
 // rewinding the polygons in case they were counter-clockwise (to avoid the black image).
   // var feat = features.features
@@ -34,19 +40,26 @@ d3.json('data/PHU.json', function(error, features) {
   // console.log(fixed);
 
 
-  projection.fitSize([width,height], features)
-
   // We add a <g> element to the SVG element and give it a class to
   // style it later.
-  svg.append('g')
-      .attr('class', 'features')
-    // D3 wants us to select the (non-existing) path objects first ...
-    .selectAll('path')
-      // ... and then enter the data. For each feature, a <path> element
-      // is added.
-      .data(features.features) // was features.features
-    .enter().append('path')
-      // As "d" attribute, we set the path of the feature.
-      .attr('d', path);
+
+
+  // svg.append('g')
+  //     .attr('class', 'features')
+  //   // D3 wants us to select the (non-existing) path objects first ...
+  //   .selectAll('path')
+  //     // ... and then enter the data. For each feature, a <path> element
+  //     // is added.
+  //     .data(features.features) // was features.features
+  //   .enter().append('path')
+  //     // As "d" attribute, we set the path of the feature.
+  //     .attr('d', path);
+
+
+  svg.selectAll("append")
+    .data(ontario.features)
+    .enter()
+    .append("path")
+    .attr("d", path);
 
 });

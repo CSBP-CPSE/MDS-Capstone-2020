0ea85c382cd6004279f7c3aa1865402aef181597
kt <kthobbs@mail.ubc.ca>
2020-06-11 12:28:38 -0700

    updated

--- d3/kt/main.js
+++ d3/kt/main.js
@@ -13,13 +13,12 @@ function InitializeMap(container, width, height) {
 	// We add a <g> element to the SVG element and give it a class to
 	// style. We also add a class name for Colorbrewer.
 	var mapFeatures = svg.append('g').attr('class', 'features YlGnBu');
-	  
+
 	// We add a <div> container for the tooltip, which is hidden by default.
 	var tooltip = d3.select("#map")
 				    .append("div")
 				    .attr("class", "tooltip hidden");
-  
-	
+
 	// We define a geographical projection
 	//     https://github.com/mbostock/d3/wiki/Geo-Projections
 	// and set the initial zoom to show the features.
@@ -32,7 +31,7 @@ function InitializeMap(container, width, height) {
 
 	// We prepare a path object and apply the projection to it.
 	var path = d3.geo.path().projection(projection);
-	
+
 	// We prepare a quantize scale to categorize the values in 9 groups.
 	// The scale returns text values which can be used for the color CSS
 	// classes (q0-9, q1-9 ... q8-9). The domain will be defined once the
@@ -52,14 +51,15 @@ function InitializeMap(container, width, height) {
 	};
 }
 
+
 function InitZoom(map) {
 	// Define the zoom and attach it to the map
 	var zoom = d3.behavior.zoom()
 				 .scaleExtent([1, 10])
 				 .on('zoom', doZoom);
-				 
+
 	map.svg.call(zoom);
-	
+
 	/**
 	 * Zoom the features on the map. This rescales the features on the map.
 	 * Keep the stroke width proportional when zooming in.
@@ -69,112 +69,109 @@ function InitZoom(map) {
 				// Keep the stroke width proportional. The initial stroke width
 				// (0.5) must match the one set in the CSS.
 				.style("stroke-width", 0.5 / d3.event.scale + "px");
+		map.mapFeatures.selectAll("circle")
+            // .attr("d", map.projection(projection))
+            .attr("r", 4/zoom.scale())
+						.attr("stroke-width", 1/zoom.scale());
 	}
 }
 
 // We specify the dimensions for the map container. We use the same
 // width and height as specified in the CSS above.
 var map1 = InitializeMap('#map', width, height);
-var map2 = InitializeMap('#map2', width, height);
-var map3 = InitializeMap('#map3', width, height);
+// var map2 = InitializeMap('#map2', width, height);
+// var map3 = InitializeMap('#map3', width, height);
 var map4 = InitializeMap('#map4', width, height);
 
 var legend1;
-var legend2;
-var legend3;
+// var legend2;
+// var legend3;
 var legend4;
 
 InitZoom(map1);
-InitZoom(map2);
+// InitZoom(map2);
+// InitZoom(map3);
+InitZoom(map4);
 
 // Load the features from the GeoJSON.
 d3.json('data/reversed_phu.geojson', function(error, collection) {
+	// console.log(collection.features)
 	var features = collection.features;
-	
+
 	// Read the data for the cartogram
-	d3.csv('data/phu_statistics.csv', function(data) { 
+	d3.csv('data/phu_statistics.csv', function(data) {
 		// We store the data object in the variable which is accessible from
-		// outside of this function.	
+		// outside of this function.
 		var index = d3.nest()
 					   .key(function(d) { return d.HR_UID; })
 					   .rollup(function(d) { return d[0]; })
 					   .map(data);
 
 		map1.features = features;
-		map2.features = features;
-		map3.features = features;
-		map4.features = features;
-		
+		// map2.features = features;
+		// map3.features = features;
+		// map4.features = features;
+
 		map1.data = data;
-		map2.data = data;
-		map3.data = data;
+		// map2.data = data;
+		// map3.data = data;
 		// map4.data = data;
-		
+
 		map1.index = index;
-		map2.index = index;
-		map3.index = index;
+		// map2.index = index;
+		// map3.index = index;
 		// map3.index = index;
 
-		LoadData(map1, 'TOTAL_prop');
-		LoadData(map2, 'TOTAL');
-		LoadData(map3, 'FATAL');
+		LoadData(map1, key = 'TOTAL_prop');
+		// LoadData(map2, key = 'TOTAL');
+		// LoadData(map3, key = 'FATAL');
 		// LoadData(map4, 'FATAL');
-		
+
 		legend1 = InitializeLegend('#legend', map1);
-		legend2 = InitializeLegend('#legend2', map2);
-		legend3 = InitializeLegend('#legend3', map3);
+		// legend2 = InitializeLegend('#legend2', map2);
+		// legend3 = InitializeLegend('#legend3', map3);
 		//legend4 = InitializeLegend('#legend4', map4);
-		
+
 		// We add a listener to the browser window, calling updateLegend when the window is resized.
-		window.onresize = (ev) => { 
-			updateLegend(map1, legend1); 
-			updateLegend(map2, legend2); 
-			//updateLegend(map3, legend3); 
-			//updateLegend(map4, legend4); 
+		window.onresize = (ev) => {
+			updateLegend(map1, legend1);
+			updateLegend(map2, legend2);
+			updateLegend(map3, legend3);
+			//updateLegend(map4, legend4);
 		};
-	}) 
+
+	})
 })
 
 
 // TODO : Map data for map2, 3, 4
-/*
+
+// Load the features from the GeoJSON.
+d3.json('data/reversed_LHIN.json', function(error, collection) {
+	// console.log(collection.features)
+	var features = collection.features;
 
 			d3.csv("data/ltc_points.csv", function(csv) {
-				// console.log(csv)
+				console.log(csv)
+						map4.features = features;
+						map4.data = csv;
+						LoadData(map4, key = 0, points = csv);
 
-						map.mapFeatures.selectAll("circle")
-							 .data(csv)
-						   .enter()
-							 .append("circle")
-							 .attr("cx", function (d) {
-								 return projection([d.long, d.lati])[0];
-							 })
-							 .attr("cy", function (d) {
-								 return projection([d.long, d.lati])[1];
-							 })
-							 // .attr("r", function (d) {
-							 //     return Math.sqrt(parseInt(d.number_beds));
-							 // })
-							 .attr("r", 2)
-							 .style("fill", status) //69b3a2
-							 .attr("stroke", status)
-							 .attr("stroke-width", 1)
-							 .attr("fill-opacity", .4)
-							 .on('mousemove', showHomes)
-							 .on('mouseout', hideHome)
-							 .on('click', showLTC)
-					});
-*/
 
-function LoadData(map, key) {
+
+				})
+	});
+
+
+function LoadData(map, key, points) {
+	points = points || 0
+	key = key || 0
 	// We add the features to the <g> element created before.
 	map.mapFeatures.selectAll('path') // D3 wants us to select the (non-existing) path objects first ...
-				   .data(map.features) // ... and then enter the data. For each feature, a <path> element is added. 
+				   .data(map.features) // ... and then enter the data. For each feature, a <path> element is added.
 				   .enter().append('path')
-				   .attr('d', map.path) // As "d" attribute, we set the path of the feature.
-				   // .on('mousemove', showTooltip) // When the mouse moves over a feature, show the tooltip.
-				   // .on('mouseout', hideTooltip) // When the mouse moves out of a feature, hide the tooltip.
-				   // .on('click', showDetails); // When a feature is clicked, show the details of it.
+	 				.attr('d', map.path) // As "d" attribute, we set the path of the feature.
+
 
 	// Set the domain of the values (the minimum and maximum values of
 	// all values of the current key) to the quantize scale.
@@ -182,20 +179,93 @@ function LoadData(map, key) {
 		d3.min(map.data, d => +d[key]),
 		d3.max(map.data, d => +d[key])
 	]);
-	
-	// Update the class (determining the color) of the features.
-	map.mapFeatures.selectAll('path')
-				   .attr('class', function(f) {
-					   var id = f.properties.HR_UID;
-					   var meta = map.index[id];
-					   
-						// Use the quantized value for the class
-						return map.quantize(+meta[key]);
-					});
 
-	// We call the function to update the legend.
-	// updateLegend();
-}
+	if (key != 0){			// Update the class (determining the color) of the features.
+			map.mapFeatures.selectAll('path')
+						   .attr('class', function(f) {
+							   var id = f.properties.HR_UID;
+							   var meta = map.index[id];
+
+								return map.quantize(+meta[key]); // Use the quantized value for the class
+							})
+	 				   .on('mousemove',  function(f){ // show tooltip
+	 						 var id = f.properties.HR_UID;
+	 						 var meta = map.index[id];
+
+	 							map.tooltip.classed('hidden', false)
+	 							.html(meta.ENG_LABEL + "</br> "
+										+ "Proportion: " + meta.TOTAL_prop + "</br> "
+										+ "Fatalities: " + meta.FATAL + "</br> "
+										+ "Amenity Score: " + meta.amenity_dense)
+	 						})
+							.on('mouseout', function() {map.tooltip.classed('hidden', true)}) //hide tooltip
+				};
+
+	if (points != 0)	{
+					map.mapFeatures.selectAll("path")
+						.attr('d', map.path)
+						.style('fill', "#5e6360") // map fill colour
+
+					map.mapFeatures.selectAll("circle")
+						 .data(points)
+						 .enter()
+						 .append("circle")
+						 .attr("cx", function (d) {
+							 return map.projection([d.long, d.lati])[0];
+						 })
+						 .attr("cy", function (d) {
+							 return map.projection([d.long, d.lati])[1];
+						 })
+						 .attr("r", 2)
+						 .style("fill", status)
+						 .attr("stroke", status)
+						 .attr("stroke-width", 1)
+						 .attr("fill-opacity", .4)
+						 .on('mousemove', showHomes)
+						 .on('mouseout', hideHome)
+					 };
+
+
+
+					 // LTC SHOW HOME NAME
+				 	function showHomes(d) {
+				 		// Get the ID of the feature.
+				 		d3.select(this).attr("r", 10)
+
+				 		// Get the current mouse position (as integer)
+				 		var mouse = d3.mouse(d3.select('#map').node()).map(
+				 			function(d) { return parseInt(d); }
+				 		);
+
+				 		// Calculate the absolute left and top offsets of the tooltip. If the
+				 		// mouse is close to the right border of the map, show the tooltip on
+				 		// the left.
+				 		var left = Math.min(width - 4 * d.cleaned_name.length, mouse[0] + 5);
+				 		var top = mouse[1] + 25;
+
+				 		// Show the tooltip (unhide it) and set the name of the data entry.
+				 		// Set the position as calculated before.
+				 		map.tooltip.classed('hidden', false)
+				 			   .attr("style", "left:" + left + "px; top:" + top + "px")
+				 			   .html(d.cleaned_name+ "</br> "
+ 										+ "Type: " + d.home_type + "</br> "
+ 										+ "Beds: " + d.number_beds + "</br> "
+ 										+ "LHIN: " + d.LHIN + "</br> "
+										+ "Status: " + d.status);
+				 	}
+
+
+					/**
+					 * LTC Hide the tooltip.
+					 */
+					function hideHome() {
+						d3.select(this).attr("r", 2)
+						map.tooltip.classed('hidden', true);
+					}
+};
+
+
+
 
 
 //  Colour by LTC home status
@@ -205,36 +275,6 @@ function status(d){
 	else return "#B51515";
 }
 
-// LTC SHOW HOME NAME
-function showHomes(d) {
-	// Get the ID of the feature.
-	d3.select(this).attr("r", 10)
-	
-	// Get the current mouse position (as integer)
-	var mouse = d3.mouse(d3.select('#map').node()).map(
-		function(d) { return parseInt(d); }
-	);
-
-	// Calculate the absolute left and top offsets of the tooltip. If the
-	// mouse is close to the right border of the map, show the tooltip on
-	// the left.
-	var left = Math.min(width - 4 * d.cleaned_name.length, mouse[0] + 5);
-	var top = mouse[1] + 25;
-
-	// Show the tooltip (unhide it) and set the name of the data entry.
-	// Set the position as calculated before.
-	tooltip.classed('hidden', false)
-		   .attr("style", "left:" + left + "px; top:" + top + "px")
-		   .html(d.cleaned_name);
-}
-
-/**
- * LTC Hide the tooltip.
- */
-function hideHome() {
-	d3.select(this).attr("r", 2)
-	tooltip.classed('hidden', true);
-}
 
 /**
  * Hide the details <div> container and show the initial content instead.
@@ -247,39 +287,10 @@ function hideDetails() {
 }
 
 /**
- * Show a tooltip with the name of the feature.
- *
- * @param {object} f - A GeoJSON Feature object.
- */
-function showTooltip(f) {
-  // Get the ID of the feature.
-  var id = getIdOfFeature(f);
-  // Use the ID to get the data entry.
-  var d = dataById[id];
-
-  // Get the current mouse position (as integer)
-  var mouse = d3.mouse(d3.select('#map').node()).map(
-    function(d) { return parseInt(d); }
-  );
-
-  // Calculate the absolute left and top offsets of the tooltip. If the
-  // mouse is close to the right border of the map, show the tooltip on
-  // the left.
-  var left = Math.min(width - 4 * d.ENG_LABEL.length, mouse[0] + 5);
-  var top = mouse[1] + 25;
-
-  // Show the tooltip (unhide it) and set the name of the data entry.
-  // Set the position as calculated before.
-  tooltip.classed('hidden', false)
-    .attr("style", "left:" + left + "px; top:" + top + "px")
-    .html(d.ENG_LABEL);
-}
-
-/**
  * Hide the tooltip.
  */
 function hideTooltip() {
-  tooltip.classed('hidden', true);
+  map.tooltip.classed('hidden', true);
 }
 
 
@@ -293,14 +304,4 @@ function hideTooltip() {
  */
 function getValueOfData(d) {
   return +d[currentKey];
-}
-
-/**
- * Helper function to retrieve the ID of a feature. The ID is found in
- * the properties of the feature.
- *
- * @param {object} f - A GeoJSON Feature object.
- */
-function getIdOfFeature(f) {
-  return f.properties.HR_UID;
 }
